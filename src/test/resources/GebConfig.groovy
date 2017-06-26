a1ae8d66e5aa6d18dfa8586eeff57bec416c688b
Dylan McClelland <dylanmccl@gmail.com>
2017-06-26 00:58:49 -0400

    add git ignore and chrome and firevox tests

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,5 +1,6 @@
 
-import org.openqa.selenium.phantomjs.PhantomJSDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 
 waiting {
@@ -7,9 +8,20 @@ waiting {
 }
 
 environments {
-	phantomJs {
-        driver = { new PhantomJSDriver() }
-    }
+	// run via “./gradlew chromeTest”
+	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
+	chrome {
+		driver = { new ChromeDriver() }
+	}
+	
+	// run via “./gradlew firefoxTest”
+	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+	firefox {
+		atCheckWaiting = 1
+
+		driver = { new FirefoxDriver() }
+	}
+	//run via "./gradlew htmlUnitTest"
 	htmlUnit{
 		driver = {new HtmlUnitDriver()}
 	}
